55c54bf554a0981b68497b817ecc9d1b36bfc27c
Zadigo <pendenquejohn@gmail.com>
2023-05-16 19:50:00 +0200

    Additional utils

--- kryptone/utils.py
+++ kryptone/utils.py
@@ -1,6 +1,13 @@
 import json
 import random
 
+
+def read_document(filename):
+    with open(filename, mode='r', encoding='utf-8') as f:
+        data = f.read()
+    return data
+
+
 def write_json_document(filename, data):
     with open(filename, mode='w+', encoding='utf-8') as f:
         # data = {'urls_to_visit': [], 'visited_urls': ['http://example.com']}
@@ -19,4 +26,11 @@ def random_user_agent(func):
         return random.choice(data)
     return wrapper
 
+
 RANDOM_USER_AGENT = random_user_agent(read_json_document)
+
+
+def drop_null(items):
+    for item in items:
+        if item is not None:
+            yield item
