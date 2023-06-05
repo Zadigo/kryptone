cfb8f9afb29d13a82b575dfecb03d95c7ec53e9f
Zadigo <pendenquejohn@gmail.com>
2023-06-05 23:07:10 +0200

    Reformat tests

--- tests/testproject/app.py
+++ tests/testproject/app.py
@@ -12,14 +12,7 @@ class Jennyfer(BaseCrawler):
     products = []
 
     def get_products(self):
-        products = self.driver.get_element_by_xpath(
-            '//div[contains(@class, "product-grid")]//div[contains(@class, "category-tile")]'
-        )
-        for product in products:
-            something = 1
-            yield {
-                'nom': None
-            }
+        return []
 
     def run_actions(self, current_url, **kwargs):
         pass
--- tests/testproject/manage.py
+++ tests/testproject/manage.py
@@ -6,5 +6,5 @@ import sys
 from kryptone.management import execute_command_inline
 
 if __name__ == '__main__':
-    os.environ.setdefault('KRYPTONE_SPIDER', 'testproject')
+    os.environ.setdefault('KRYPTONE_SPIDER', 'tests.testproject')
     execute_command_inline(sys.argv)
