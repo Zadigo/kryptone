2db93b30732f461aaaa9748e01e4be2c72158bf4
Zadigo <pendenquejohn@gmail.com>
2023-06-21 15:38:17 +0200

    Include automating classes

--- kryptone/conf/base.py
+++ kryptone/conf/base.py
@@ -8,17 +8,31 @@ GLOBAL_KRYPTONE_PATH = pathlib.Path(__file__).parent.parent.absolute()
 PROJECT_PATH = None
 
 
+# Register spiders to crawl
+# the internet
 SPIDERS = []
 
 
-# The webdriver browser to use
+# Register spiders to automate
+# actions on different websites
+AUTOMATERS = []
+
+
+# The browser to use
 WEBDRIVER = {
     'driver': 'selenium.webdriver.Edge',
     'executable_path': None,
 }
 
 
+# The folder in which to save
+# media files
 MEDIA_FOLDER = 'media'
+
+
+# The amount of time the driver should
+# wait before moving to the next url
+WAIT_TIME = 25
 
 
 # The name of the cache file
