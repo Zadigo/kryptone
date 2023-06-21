1c8b6c5d2c03e6deb1d1594113e7ae6c774f1d22
Zadigo <pendenquejohn@gmail.com>
2023-06-21 15:38:36 +0200

    Code refactoring

--- kryptone/crawlers/google_maps.py
+++ kryptone/crawlers/google_maps.py
@@ -86,9 +86,6 @@ class GoogleMapsMixin:
 class GoogleMaps(GoogleMapsMixin, SinglePageAutomater):
     final_result = []
 
-    # start_url = "https://www.google.com/maps/search/Marie+Blach%C3%A8re/@44.2399522,1.6699718,8.75z?entry=ttu"
-    # start_url = generate_search_url('la mie câline')
-
     def post_visit_actions(self, **kwargs):
         try:
             # Google has a special consent form
@@ -669,24 +666,24 @@ class GoogleMapsPlace(GoogleMaps):
         # completion_time = (time.time() - current_time) / 60
 
 
-if __name__ == '__main__':
-    # try:
-    #     instance = GoogleMapsPlace()
-    #     # instance = GoogleMaps()
-    #     urls = [
-    #         'https://www.google.fr/maps/place/Bricomarch%C3%A9/@45.0623843,5.0824153,13z/data=!4m10!1m2!2m1!1sbricomarch%C3%A9!3m6!1s0x478ab2c99e97aba7:0xf9a321930a23c394!8m2!3d45.05827!4d5.107725!15sCgxicmljb21hcmNow6kiA4gBAZIBFGRvX2l0X3lvdXJzZWxmX3N0b3Jl4AEA!16s%2Fg%2F1w8w8xmm?entry=ttu'
-    #     ]
-    #     instance.start(start_urls=urls, wait_time=1)
-    # except KeyboardInterrupt:
-    #     data = list(map(lambda x: x.as_json, instance.final_result))
-    #     write_json_document('dump.json', data)
-    #     logger.critical(f"Dumping data to 'dump.json'")
-    # except Exception:
-    #     data = list(map(lambda x: x.as_json, instance.final_result))
-    #     write_json_document('dump.json', data)
-    #     logger.critical(f"Dumping data to 'dump.json'")
-    #     raise
+# if __name__ == '__main__':
+#     # try:
+#     #     instance = GoogleMapsPlace()
+#     #     # instance = GoogleMaps()
+#     #     urls = [
+#     #         'https://www.google.fr/maps/place/Bricomarch%C3%A9/@45.0623843,5.0824153,13z/data=!4m10!1m2!2m1!1sbricomarch%C3%A9!3m6!1s0x478ab2c99e97aba7:0xf9a321930a23c394!8m2!3d45.05827!4d5.107725!15sCgxicmljb21hcmNow6kiA4gBAZIBFGRvX2l0X3lvdXJzZWxmX3N0b3Jl4AEA!16s%2Fg%2F1w8w8xmm?entry=ttu'
+#     #     ]
+#     #     instance.start(start_urls=urls, wait_time=1)
+#     # except KeyboardInterrupt:
+#     #     data = list(map(lambda x: x.as_json, instance.final_result))
+#     #     write_json_document('dump.json', data)
+#     #     logger.critical(f"Dumping data to 'dump.json'")
+#     # except Exception:
+#     #     data = list(map(lambda x: x.as_json, instance.final_result))
+#     #     write_json_document('dump.json', data)
+#     #     logger.critical(f"Dumping data to 'dump.json'")
+#     #     raise
 
-    instance = GoogleMapsPlace()
-    url_file = URLFile(processor=generate_search_url)
-    instance.start(start_urls=list(url_file))
+#     instance = GoogleMapsPlace()
+#     url_file = URLFile(processor=generate_search_url)
+#     instance.start(start_urls=list(url_file))
