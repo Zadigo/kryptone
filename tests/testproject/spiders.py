dcda0c2cf9780fbc94f647a2610b1e386f30c6c3
Zadigo <pendenquejohn@gmail.com>
2023-06-22 14:22:30 +0200

    Improve product tests

--- tests/testproject/spiders.py
+++ tests/testproject/spiders.py
@@ -9,15 +9,9 @@ class Jennyfer(BaseCrawler):
     in the browser to extract data"""
 
     start_url = 'https://www.jennyfer.com/fr-fr/vetements/maillots-de-bain/'
-    products = []
 
     def get_products(self):
         return []
 
     def run_actions(self, current_url, **kwargs):
         pass
-
-
-if __name__ == '__main__':
-    instance = Jennyfer()
-    instance.start()
