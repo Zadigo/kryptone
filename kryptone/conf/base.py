e8ed83f943027bbec6ac0ad78ec7d8fef110fc6b
Zadigo <pendenquejohn@gmail.com>
2023-06-22 14:20:21 +0200

    Improve settings document

--- kryptone/conf/base.py
+++ kryptone/conf/base.py
@@ -43,3 +43,55 @@ WAIT_TIME = 25
 #     'default': 'kryptone.cache.Cache',
 #     'location': None
 # }
+
+
+# Additional storage backends to use
+ACTIVE_STORAGE_BACKENDS = []
+
+
+# External storage backends to use to save the
+# data gathered by the spiders. The default storage
+# method is a JSON file
+STORAGE_BACKENDS = {
+    'airtable': {
+        'type': 'online',
+        'credentials': {
+            'API_KEY': None,
+            'BASE_ID': None,
+            'TABLE_NAME': None
+        }
+    },
+    'google_sheets': {
+        'type': 'online',
+        'credentials': {
+            'KEY': None,
+            'item_name': None,
+            'item_id': None
+        }
+    },
+    'notion': {
+        'type': 'online',
+        'credentials': {
+            'TOKEN': None,
+            'DATABASE_ID': None
+        }
+    }
+}
+
+
+# Email setting values used essentially
+# for alerting users for failed events
+# or sending captured data
+EMAIL_HOST = 'smtp.gmail'
+
+EMAIL_HOST = None
+
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = None
+
+EMAIL_HOST_PASSWORD = None
+
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = None
