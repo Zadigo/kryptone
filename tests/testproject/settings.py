3bed974db7ac5a953e78419c2b0282af04a0a351
Zadigo <pendenquejohn@gmail.com>
2023-06-22 15:13:14 +0200

    Update test project settings

--- tests/testproject/settings.py
+++ tests/testproject/settings.py
@@ -1,27 +1,48 @@
 import pathlib
 
+
 # Absolute path to the local project
-PROJECT_PATH = pathlib.Path(__file__).resolve().parent
+PROJECT_PATH = None
 
 
+# Register spiders to crawl
+# the internet
 SPIDERS = [
     'Jennyfer'
 ]
 
+
+# Register spiders to automate
+# actions on different websites
 AUTOMATERS = []
 
 
-# The webdriver browser to use
-WEBDRIVER = {
-    'driver': 'selenium.webdriver.Edge',
-    'executable_path': None,
-}
+# The browser to use
+WEBDRIVER = 'selenium.webdriver.Chrome'
 
 
+# The folder in which to save
+# media files
 MEDIA_FOLDER = 'media'
 
 
-WAIT_TIME = 15
+# The amount of time the driver should
+# wait before moving to the next url
+WAIT_TIME = 25
+
+
+# The name of the cache file
+# CACHE_FILE_NAME = 'cache.json'
+
+
+# CACHE = {
+#     'default': 'kryptone.cache.Cache',
+#     'location': None
+# }
+
+
+# Additional storage backends to use
+ACTIVE_STORAGE_BACKENDS = []
 
 
 # External storage backends to use to save the
