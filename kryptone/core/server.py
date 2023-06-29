fe3ab6a6260566c4564307a7eb5b55b617aaac13
Zadigo <pendenquejohn@gmail.com>
2023-06-29 20:22:52 +0200

    Improve task creation

--- kryptone/core/server.py
+++ kryptone/core/server.py
@@ -1,7 +1,7 @@
 import subprocess
 import threading
 import time
-
+from functools import lru_cache
 from kryptone import logger
 from kryptone.db.connections import redis_connection
 from kryptone.signals import Signal
@@ -28,20 +28,41 @@ class TaskServer(BaseServer):
         """Checks the Redis backend to see
         if there are tasks that need to
         be executed"""
-        return len(self.tasks) > 0
+        return len(self.tasks()) > 0
 
-    @property
+    @lru_cache(maxsize=100)
     def tasks(self):
-        try:
-            tasks = self.connection.get('kryptone_tasks')
-        except:
-            return []
-        else:
-            return tasks or []
+        # Scan the redis database for all
+        # available tasks
+        _, keys = self.connection.scan()
+        filtered_tasks = filter(
+            lambda task_name: task_name.decode('utf-8').startswith('task_'),
+            keys
+        )
+        return list(filtered_tasks)
+
+    def tasks_to_execute(self):
+        tasks = []
+        for task_name in self.tasks():
+            state = self.connection.hget(task_name, 'completed')
+            if not state:
+                continue
+            tasks.append(task_name)
+
+    # def execute_tasks(self):
+    #     threads = []
+    #     for task_name in self.tasks_to_execute():
+    #         threads.append(
+    #             threading.Thread(
+    #                 target=None,
+    #                 name=task_name
+    #             )
+    #         )
 
     def run(self, *args, **kwargs):
         if self.has_tasks:
-            print(self.tasks)
+            # subprocess.call()
+            logger.info(self.tasks())
         time.sleep(3)
 
 
@@ -60,6 +81,3 @@ def run_server(*args, **kwargs):
     kryptone_main_thread.start()
     while True:
         kryptone_main_thread.join()
-
-
-run_server()
