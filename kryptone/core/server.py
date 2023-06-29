93035d42db167142aa4ad2233345b2e90effdbc5
Zadigo <pendenquejohn@gmail.com>
2023-06-29 18:53:10 +0200

    Improve servers

--- kryptone/core/server.py
+++ kryptone/core/server.py
@@ -1,5 +1,6 @@
 import subprocess
 import threading
+import time
 
 from kryptone import logger
 from kryptone.db.connections import redis_connection
@@ -7,29 +8,46 @@ from kryptone.signals import Signal
 
 server_started = Signal()
 
+
 class BaseServer:
     def __init__(self):
-        self.trigger = False
-        self.connection = redis_connection()
         logger.info('Starting server...')
         server_started.send(self)
 
-    def run(self):
-        if self.connection:
-            self.trigger = self.connection.get('project')
+    def run(self, *args, **kwargs):
+        pass
 
-        if self.trigger:
-            cmd = ['python', 'manage.py', 'start']
-            subprocess.call(cmd)
-            if self.connection:
-                self.connection.set('project', False)
+
+class TaskServer(BaseServer):
+    def __init__(self):
+        super().__init__()
+        self.connection = redis_connection()
+
+    @property
+    def has_tasks(self):
+        """Checks the Redis backend to see
+        if there are tasks that need to
+        be executed"""
+        return len(self.tasks) > 0
+
+    @property
+    def tasks(self):
+        try:
+            return self.connection.get('kryptone_tasks')
+        except:
+            return []
+
+    def run(self, *args, **kwargs):
+        if self.has_tasks:
+            print(self.tasks)
+        time.sleep(3)
 
 
 def run_server(*args, **kwargs):
     """Starts the server for Kryptone. This
     will essentially poll the database backend
     for a trigger to launch the web scrapper"""
-    server = BaseServer()
+    server = TaskServer()
     kryptone_main_thread = threading.Thread(
         target=server.run,
         args=args,
@@ -40,3 +58,6 @@ def run_server(*args, **kwargs):
     kryptone_main_thread.start()
     while True:
         kryptone_main_thread.join()
+
+
+run_server()
