f0a2132b6a02b9a0759978314621d32f0ef53841
Zadigo <pendenquejohn@gmail.com>
2023-07-23 22:50:30 +0200

    Improve logging

--- kryptone/contrib/models.py
+++ kryptone/contrib/models.py
@@ -10,18 +10,18 @@ class BaseModel:
         """Get the fields present on the model"""
         fields = dataclasses.fields(self)
         return list(map(lambda x: x.name, fields))
-    
+
     @cached_property
     def url_object(self):
         return urlparse(getattr(self, 'url', ''))
-    
+
     def as_json(self):
         """Return the object as dictionnary"""
         item = {}
         for field in self.fields:
             item[field] = getattr(self, field)
         return item
-    
+
     def as_csv(self):
         def convert_values(field):
             value = getattr(self, field)
@@ -44,7 +44,7 @@ class Product(BaseModel):
     # number_of_colors: int = 1
     # images:str = dataclasses.field(default_factory=[])
     # color: str = None
-    
+
     def get_collection_id(self, regex):
         result = re.search(regex, getattr(self, 'url'))
         if result:
