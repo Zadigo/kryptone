64084abca69687c3d6a8ee9d4388bc28860bb0f9
Zadigo <pendenquejohn@gmail.com>
2023-09-08 09:49:17 +0200

    Remove db_signals

--- kryptone/db/backends.py
+++ kryptone/db/backends.py
@@ -3,14 +3,14 @@ import airtable
 import gspread
 import requests
 
-from kryptone.base import db_signal
+# from kryptone.base import db_signal
 from kryptone.conf import settings
 from kryptone.db.connections import redis_connection
-from kryptone.signals import function_to_receiver
+# from kryptone.signals import function_to_receiver
 
 AIRTABLE_ID_CACHE = set()
 
-@function_to_receiver(db_signal)
+# @function_to_receiver(db_signal)
 def airtable_backend(sender, **kwargs):
     """Use Airtable as a storage backend"""
     if 'airtable' in settings.ACTIVE_STORAGE_BACKENDS:
