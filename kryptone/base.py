a06e3cefbd2863b75bfd9f795fc96543fdbb5ad2
Zadigo <pendenquejohn@gmail.com>
2023-10-12 10:04:42 +0200

    Bug fix on suffix

--- kryptone/base.py
+++ kryptone/base.py
@@ -396,7 +396,7 @@ class BaseCrawler(metaclass=Crawler):
             if self._meta.ignore_images:
                 path = pathlib.Path(link)
                 if path != '':
-                    if path in self.default_image_extensions:
+                    if path.suffix in self.default_image_extensions:
                         continue
 
             self.urls_to_visit.add(link)
