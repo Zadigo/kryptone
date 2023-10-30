bb26a8a46e633601ecaa0e6ec2003e3a88f25f98
Zadigo <pendenquejohn@gmail.com>
2023-10-30 12:24:12 +0100

    Add migration commands

---
+++ kryptone/management/commands/migrate.py
@@ -0,0 +1,13 @@
+import kryptone
+from kryptone.management.base import ProjectCommand
+from kryptone.checks.core import checks_registry
+from kryptone.contrib.database import database
+
+
+class Command(ProjectCommand):
+    requires_system_checks = True
+
+    def execute(self, namespace):
+        kryptone.setup()
+        checks_registry.run()
+        database.migrate()
