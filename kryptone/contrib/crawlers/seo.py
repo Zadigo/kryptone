baade40d01d261d0ce13f9e9f2325f839605f426
Zadigo <pendenquejohn@gmail.com>
2023-11-10 23:46:01 +0100

    Make element asynchronous

--- kryptone/contrib/crawlers/seo.py
+++ kryptone/contrib/crawlers/seo.py
@@ -2,6 +2,7 @@ from kryptone import logger
 from kryptone.base import SiteCrawler
 from kryptone.mixins import SEOMixin
 from kryptone.utils import file_readers
+import asyncio
 
 
 class SEOCrawler(SiteCrawler, SEOMixin):
@@ -28,29 +29,38 @@ class SEOCrawler(SiteCrawler, SEOMixin):
     
     def run_actions(self, current_url, **kwargs):
         self.audit_page(current_url)
-        file_readers.write_json_document('audit.json', self.page_audits)
-
-        # Write vocabulary as JSON
         vocabulary = self.global_audit()
-        file_readers.write_json_document('global_audit.json', vocabulary)
 
-        # Write vocabulary as CSV
-        rows = []
-        for key, value in vocabulary.items():
-            rows.append([key, value])
-        file_readers.write_csv_document('global_audit.csv', rows)
+        async def write_audit_documents():
+            file_readers.write_json_document('audit.json', self.page_audits)
+            file_readers.write_json_document('global_audit.json', vocabulary)
 
-        # Save the website's text
-        website_text = ' '.join(self.fitted_page_documents)
-        file_readers.write_text_document(
-            'website_text.txt', 
-            website_text
-        )
+        async def write_vocabulary():
+            # Write vocabulary as CSV
+            rows = []
+            for key, value in vocabulary.items():
+                rows.append([key, value])
+            file_readers.write_csv_document('global_audit.csv', rows)
 
-        file_readers.write_json_document(
-            'text_by_pages.json',
-            self.text_by_page
-        )
+        async def write_website_text():
+            # Save the website's text
+            website_text = ' '.join(self.fitted_page_documents)
+            file_readers.write_text_document(
+                'website_text.txt', 
+                website_text
+            )
+
+            file_readers.write_json_document(
+                'text_by_pages.json',
+                self.text_by_page
+            )
+
+        async def main():
+            coroutines = [write_audit_documents(), write_vocabulary(), write_website_text()]
+            for coroutine in asyncio.as_completed(coroutines):
+                await coroutine
+
+        asyncio.run(main())
         
         # db_signal.send(
         #     self,
