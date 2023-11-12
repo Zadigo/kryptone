ceedb192fe01a9b00b072a4ca8ad76ae675bd242
Zadigo <pendenquejohn@gmail.com>
2023-11-12 22:43:40 +0100

    Improve file writing functions

--- kryptone/contrib/crawlers/seo.py
+++ kryptone/contrib/crawlers/seo.py
@@ -1,4 +1,5 @@
 import asyncio
+from collections import defaultdict
 
 from kryptone import logger
 from kryptone.base import SiteCrawler
@@ -21,7 +22,9 @@ class SEOCrawler(SiteCrawler, SEOMixin):
         self.visited_urls = set(data['visited_urls'])
 
         previous_seen_urls = file_readers.read_csv_document(
-            'seen_urls.csv', flatten=True)
+            'seen_urls.csv', 
+            flatten=True
+        )
         self.list_of_seen_urls = set(previous_seen_urls)
 
         self.page_audits = file_readers.read_json_document('audit.json')
@@ -32,22 +35,21 @@ class SEOCrawler(SiteCrawler, SEOMixin):
 
     def run_actions(self, current_url, **kwargs):
         self.audit_page(current_url)
-        vocabulary = self.global_audit()
 
         async def write_audit_documents():
+            file_readers.write_json_document('word_frequency.json', self.word_frequency_by_page)
             file_readers.write_json_document('audit.json', self.page_audits)
-            file_readers.write_json_document('global_audit.json', vocabulary)
 
         async def write_vocabulary():
             # Write vocabulary as CSV
             rows = []
-            for key, value in vocabulary.items():
-                rows.append([key, value])
-            file_readers.write_csv_document('global_audit.csv', rows)
+            for word, value in self.website_word_frequency.items():
+                rows.append([word, value])
+            file_readers.write_csv_document('word_count.csv', rows)
 
         async def write_website_text():
             # Save the website's text
-            website_text = ' '.join(self.fitted_page_documents)
+            website_text = ''.join(self.fitted_page_documents)
             file_readers.write_text_document(
                 'website_text.txt',
                 website_text
@@ -57,6 +59,17 @@ class SEOCrawler(SiteCrawler, SEOMixin):
                 'text_by_pages.json',
                 self.text_by_page
             )
+
+            file_readers.write_json_document(
+                'website_tokens.json',
+                list(self.website_tokens)
+            )
+
+            file_readers.write_json_document(
+                'stemmed_words.json',
+                list(self.stemmed_tokens)
+            )
+
 
         async def main():
             task1 = asyncio.create_task(write_audit_documents())
