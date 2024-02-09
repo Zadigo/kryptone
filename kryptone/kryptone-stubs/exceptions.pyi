d13aec955312e8008e31f430855bbd941c994322
Zadigo <pendenquejohn@gmail.com>
2024-02-09 22:45:14 +0100

    Improve stubs

--- kryptone/kryptone-stubs/exceptions.pyi
+++ kryptone/kryptone-stubs/exceptions.pyi
@@ -4,3 +4,15 @@ class ProjectExistsError(Exception):
 
 class SpiderExecutionError(Exception):
     ...
+
+
+class SpiderExistsError(Exception):
+    ...
+
+
+class BadImplementationError(Exception):
+    ...
+
+
+class NoStartUrlsFile(Exception):
+    ...
