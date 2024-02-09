fc21a88bd840211cb7ec69e68bf8bd08155cbb04
Zadigo <pendenquejohn@gmail.com>
2024-02-09 22:45:30 +0100

    Remove test receivers

--- kryptone/receivers.py
+++ kryptone/receivers.py
@@ -5,12 +5,9 @@ from kryptone.utils.iterators import JPEGImagesIterator
 
 
 def collect_images_receiver(sender, current_url=None, **kwargs):
-    """Collects every images present on the
-    actual webpage and classifies them"""
-    try:
-        image_elements = sender.driver.find_elements(By.TAG_NAME, 'img')
-    except:
-        pass
-    else:
-        instance = JPEGImagesIterator(current_url, image_elements)
-        logger.info(f'Collected {len(instance)} images')
+    """Collects every images present on the actual webpage 
+    and classifies them"""
+    pass
+    # images = sender.driver.execute_script("""return document.querySelectorAll('img')""")
+    # instance = JPEGImagesIterator(current_url, images)
+    # logger.info(f'Collected {len(instance)} image(s)')
