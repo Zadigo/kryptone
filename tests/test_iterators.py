d8ef2d10e2094922f7b0ae5436fd0312ed78e698
Zadigo <pendenquejohn@gmail.com>
2024-11-29 16:04:28 +0100

    Improve tests

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -1,5 +1,5 @@
 import unittest
-from kryptone.utils.urls import URL
+from kryptone.utils.urls import URLPaginationGenerator, URLPathGenerator, URLQueryGenerator
 
 START_URLS = [
     'http://example.com',
@@ -8,46 +8,33 @@ START_URLS = [
 
 
 class TestPagination(unittest.TestCase):
-    def test_generation(self):
-        instance = PagePaginationGenerator('http://example.com', k=1)
+    def test_generator(self):
+        instance = URLPaginationGenerator('http://example.com', k=1)
         self.assertListEqual(list(instance), ['http://example.com?page=1'])
 
-    def test_addition(self):
-        instance1 = PagePaginationGenerator('http://example.com', k=1)
-        instance2 = PagePaginationGenerator('http://google.com', k=1)
-        combinator = instance1 + instance2
-        self.assertListEqual(
-            combinator.urls,
-            ['http://example.com?page=1', 'http://google.com?page=1']
+
+class TestURLPathGenerator(unittest.TestCase):
+    def test_generator(self):
+        instance = URLPathGenerator(
+            'http://example.com/$id',
+            params={'id': 'number'},
+            k=1
+        )
+        self.assertListEqual(list(instance), ['http://example.com/1'])
+
+
+class TestURLQueryGenerator(unittest.TestCase):
+    def test_generator(self):
+        instance = URLQueryGenerator(
+            'http://example.com/',
+            param='year',
+            param_values=['2001', '2002']
         )
 
-
-class TestUrlIterator(unittest.TestCase):
-    def setUp(self):
-        self.instance = URLIterator(start_urls=START_URLS)
-
-    def test_loop(self):
-        while not self.instance.empty:
-            url = self.instance.get()
-            with self.subTest(url=url):
-                self.assertIsInstance(url, URL)
-
-    def test_append(self):
-        self.instance.append('http://google.com')
-        self.assertFalse(self.instance.empty)
-        self.assertIn('http://google.com', self.instance)
-        self.assertTrue(len(self.instance) == 3)
-
-    def test_get(self):
-        url = self.instance.get()
-        self.assertIsInstance(url, URL)
-        self.assertTrue(self.instance.urls_to_visit_count, 1)
-
-    def test_appendleft(self):
-        url = 'http://google.com'
-        self.instance.appendleft(url)
-        self.assertTrue(self.instance[0], url)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertListEqual(
+            list(instance),
+            [
+                'http://example.com/?year=2001',
+                'http://example.com/?year=2002'
+            ]
+        )
